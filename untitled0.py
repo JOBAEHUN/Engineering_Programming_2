4e550e6b9e04142a662ba3c899afa573174bfa21
HuniJo <jbh12132@hanyang.ac.kr>
2025-03-14 15:53:24 +0900

    Auto commit from Spyder

---
+++ untitled0.py
@@ -0,0 +1,9 @@
+import os
+
+# Git 프로젝트 폴더로 이동 (본인 경로 수정)
+os.chdir("E:/MyPythonProject")  # 여기를 본인 프로젝트 폴더로 변경!
+
+# Git 자동 동기화 실행
+os.system("git add .")
+os.system('git commit -m "Auto commit from Spyder"')
+os.system("git push origin master")  # master 브랜치인 경우
