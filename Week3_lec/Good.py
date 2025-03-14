ffb84146e1ea08e25acc9c50532d7afeacfac612
HuniJo <jbh12132@hanyang.ac.kr>
2025-03-14 15:57:30 +0900

    Auto commit from Spyder

--- Week3_lec/Good.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 14 15:54:30 2025
-
-@author: jbh12
-"""
-
-아주 좋아요
--- Week3_lec/nice.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 14 15:55:48 2025
-
-@author: jbh12
-"""
-
-Nice
