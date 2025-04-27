c924888ef82b2e584e4dbde938c7a603b57cdf9b
HuniJo <jbh12132@hanyang.ac.kr>
2025-04-28 00:52:12 +0900

    Auto commit from Spyder

--- Week2/hw_2.py
+++
@@ -1,9 +0,0 @@
-start, end, step = map(int,input("세 개의 숫자를 입력하세요: ").split()) # 띄어쓰기를 기준으로 입력받기, map함수로 리스트에 넣기
-total = 0 # total 변수 0으로 초기화
-numbers =[]
-
-for i in range(start,end +1, step): # end+1까지 range를 설정해 end까지 연산이 가능하도록 하기
-    total += i
-    numbers.append(str(i)) # i 값을 문자열로 변환하여 리스트에 추가
-result = " + ".join(numbers)  # 리스트를 문자열로 변환하여 출력 포맷 생성
-print(f"{result} = {total}입니다")
--- Week2/hw_3.py
+++
@@ -1,5 +0,0 @@
-# 구구단을 하기 위한 알고리즘 for loop 2번 쓰기
-for i in range(1,10):
-    print("##{}단##".format(i))
-    for j in range(1,10):
-        print("{0} x {1} = {2}".format(i,j,i*j))
